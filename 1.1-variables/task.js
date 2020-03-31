817642465de20ff3bc8cbc78796cf8c57ef5e724
Kristina Kovpaeva <9118441339@mail.ru>
2020-03-31 11:35:11 +0300

    task commit

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -7,21 +7,35 @@
 
 function averageMark(a, g, p) {
 	
-    // реализуйте здесь задание №1
-    
-	return // ваш ответ;
+	let algebra = a;
+	let geography = g;
+	let physics = p;
+
+	let average = (algebra + geography + physics) / 3;
+
+	return average; 
 }
 
 function sayHello(userName) {
 	
-    // реализуйте здесь задание №2
-    
-	return // ваш ответ;
+	let myName = userName;
+	let message = ('Привет, мир! Меня зовут ' + myName);
+
+	return message; 
 }
 
 function calculateFormula() {
     
-	// реализуйте здесь задание №3
-    
-    return // ваш ответ;
+	let x;
+	let y;
+	let z;
+	x = 2;
+	y = 22;
+	z = 0;
+
+	let result 
+	result = (x * y + 5 * z + x - 1); 
+
+	return result; 
+	
 }
