59bd839d8f80efbedb401eefc0a142e33ff67ae2
Kristina Kovpaeva <9118441339@mail.ru>
2020-04-05 12:37:39 +0300

    Edits

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -2,15 +2,13 @@
 
 function getResult(a, b, c) {
     let x = [];
-    let D = Math.pow(b, 2) - 4 * a * c;
+    let discriminant = Math.pow(b, 2) - 4 * a * c;
     
-    if (D < 0) {
-        x = [];
-    } else if (D === 0) {
+    if (discriminant === 0) {
         x[0] = -b / (2 * a);
     } else {
-        x[0] = (-b + Math.sqrt(D)) / (2 * a);
-        x[1] = (-b - Math.sqrt(D)) / (2 * a);
+        x[0] = (-b + Math.sqrt(discriminant)) / (2 * a);
+        x[1] = (-b - Math.sqrt(discriminant)) / (2 * a);
     };
 
     return x;
@@ -18,32 +16,26 @@ function getResult(a, b, c) {
 
 function getAverageMark(marks) {
     let sum = 0;
-    let averageMark = 0;
+    let averageMark;
 
     if (marks.length === 0) {
         averageMark = 0;
-    } else if (marks.length <= 5) {
-        for (let i = 0; i < marks.length; i++) {
-            sum += marks[i];
-            averageMark = sum / marks.length;
-        }
     } else {
       marks.splice(5);
       for (let i = 0; i < marks.length; i++) {
         sum += marks[i];
-        averageMark = sum / marks.length;
       }
+      averageMark = sum / marks.length;
     }
     
     return averageMark;
 }
 
 function askDrink(name,dateOfBirthday) {
-   let nowDate = new Date();
-   let age = nowDate.getFullYear() - dateOfBirthday.getFullYear();
+//    let age = new Date().getFullYear() - dateOfBirthday.getFullYear();
    let result;
 
-   if (age >= 18) {
+   if (new Date().getFullYear() - dateOfBirthday.getFullYear() >= 18) {
        result = `Не желаете ли олд-фэшн, ${name}?`;
    } else {
        result = `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
