017917dbffd2b9aa74cd51d98c43a20f7d62a0f5
Kristina Kovpaeva <9118441339@mail.ru>
2020-04-07 12:47:49 +0300

    Second Edits

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -6,7 +6,7 @@ function getResult(a, b, c) {
     
     if (discriminant === 0) {
         x[0] = -b / (2 * a);
-    } else {
+    } else if (discriminant > 0) {
         x[0] = (-b + Math.sqrt(discriminant)) / (2 * a);
         x[1] = (-b - Math.sqrt(discriminant)) / (2 * a);
     };
@@ -16,29 +16,25 @@ function getResult(a, b, c) {
 
 function getAverageMark(marks) {
     let sum = 0;
-    let averageMark;
-
+    
     if (marks.length === 0) {
-        averageMark = 0;
+        return 0;
     } else {
       marks.splice(5);
       for (let i = 0; i < marks.length; i++) {
         sum += marks[i];
       }
-      averageMark = sum / marks.length;
+      return sum / marks.length;
     }
     
-    return averageMark;
 }
 
 function askDrink(name,dateOfBirthday) {
-//    let age = new Date().getFullYear() - dateOfBirthday.getFullYear();
-   let result;
 
    if (new Date().getFullYear() - dateOfBirthday.getFullYear() >= 18) {
-       result = `Не желаете ли олд-фэшн, ${name}?`;
+       return `Не желаете ли олд-фэшн, ${name}?`;
    } else {
-       result = `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+       return `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
    }
-     return result;
+
 }
