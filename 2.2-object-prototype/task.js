9fef943f77a479f85e270427fa0e178266cf3816
Kristina Kovpaeva <9118441339@mail.ru>
2020-04-10 11:23:54 +0300

    First Commit

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,14 +1,26 @@
 function getAnimalSound(animal) {
-    // код для задачи №1 писать здесь
-    // return animalSound;
-}
+    let sound = animal.sound;
+    if (animal == undefined) {
+        return null;
+    } else {
+        return sound;
+    }
+        
+};
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
-}
+    let sum = 0;
+    for (let i = 0; i < marks.length; i++) {
+        sum += marks[i]; 
+    };
+    return Math.round(sum / marks.length);
+};
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    let now = new Date().getTime();
+    let date = new Date(birthday).getTime();
+    let diff = now - birthday;
+    let age = Math.floor(diff / (8766*60*60*1000));
+    return age;
+    
 }
